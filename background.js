29c58ceaff84cd524a350af8ea0a608346b3e019
Zackn9ne <zackn9ne@gmail.com>
2014-07-25 22:25:17 -0400

    hustle cloud working icon does change but basic icon does not show up

---
+++ background.js
@@ -0,0 +1,23 @@
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+var min = 1;
+var max = 5;
+var current = min;
+
+function loadSoundcloud() {
+  window.open("http://www.soundcloud.com","_new");
+//}
+
+//function updateIcon() {
+  chrome.browserAction.setIcon({path:"icon" + current + ".png"});
+  current++;
+
+  if (current > max)
+    current = min;
+}
+
+chrome.browserAction.onClicked.addListener(loadSoundcloud || updateIcon);
+updateIcon();
+//loadSoundcloud();
